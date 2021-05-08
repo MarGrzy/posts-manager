e7d717939888b2191e321e7ec561a947a63a8a60
Marcin <marcin.grzymowicz94@gmail.com>
2021-05-08 14:45:50 +0200

    Initial commit.

---
+++ src/test/java/marcin/grzymowicz/posts_manager/PostsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package marcin.grzymowicz.posts_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
